be4649fc18454952c21a13cb0b63efc9b37b3356
Tyrese <jrackfinn@gmail.com>
2022-06-24 08:03:56 -0500

    inventory update

--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, OnInit,ViewChild } from '@angular/core';
 import { DataService } from '../data.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
+
 
 @Component({
   selector: 'app-inventory',
@@ -7,15 +11,55 @@ import { DataService } from '../data.service';
   styleUrls: ['./inventory.component.css']
 })
 export class InventoryComponent implements OnInit {
+  displayedColumns: string[] = ['id','name','description', 'price', 'quantity', 'action' ];
+  dataSource!: MatTableDataSource<any>;
+  cardInfo = [];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   products:any = [];
   constructor(private dataservice: DataService) { }
 
   ngOnInit() {
-    this.dataservice.sendGetRequest().subscribe((data:any[]) => {
-      this.products = data;
-      
-    })
+    this.getInventoryInfo()
+   
   }
 
+  getInventoryInfo() {
+    // this.dataservice.sendGetRequest().subscribe((data:any[]) => {
+    //   this.products = data;
+    //   this.dataSource = new MatTableDataSource(res);
+    //     this.dataSource.paginator = this.paginator
+    //     this.dataSource.sort = this.sort
+
+        this.dataservice.sendGetRequest().subscribe({
+          next: (res) => {
+            console.log(res)
+            this.dataSource = new MatTableDataSource(res);
+            this.dataSource.paginator = this.paginator
+            this.dataSource.sort = this.sort
+          },
+          error: (err) => {
+            alert("Error while fetching the records")
+    
+          }
+      })
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
+
+
