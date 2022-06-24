bdbc2c003ad2fbd29a6637ba66b9d3603569f24c
aldaine-AG <aldaineclarke1@gmail.com>
2022-06-24 15:24:17 -0500

    Updated the service file to use the product interface for all returned observables

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -8,17 +8,17 @@ import { Observable } from 'rxjs';
 export class DataService {
   private REST_API_SERVER = 'http://localhost:3000/products';
   constructor(private httpClient: HttpClient) { }
-  public sendGetRequest() {
-    return this.httpClient.get<any[]>(this.REST_API_SERVER);
+  public sendGetRequest():Observable<ProductInterface[]>{
+    return this.httpClient.get<ProductInterface[]>(this.REST_API_SERVER);
   }
 
-  public addproduct(data: any) {
-    return this.httpClient.post<any[]>(this.REST_API_SERVER, data)
+  public addproduct(data: any):Observable<ProductInterface>{
+    return this.httpClient.post<ProductInterface>(this.REST_API_SERVER, data)
     
   }
 
-  public updateproduct(data:any, id:number ) {
-    return this.httpClient.patch<any[]>(this.REST_API_SERVER+"/"+id, data);
+  public updateproduct(data:any, id:number ):Observable<ProductInterface> {
+    return this.httpClient.patch<ProductInterface>(this.REST_API_SERVER+"/"+id, data);
   }
 
   public getLimitedProducts(page?:number, limit?:number):Observable<ProductInterface[]>{
@@ -37,8 +37,8 @@ export class DataService {
 
   //FOR WHERE YOU SEE 
 
-  public fetchItem(id: Number){
-    return this.httpClient.get<any[]>(`${this.REST_API_SERVER}/${id}`);
+  public fetchItem(id: Number):Observable<ProductInterface>{
+    return this.httpClient.get<ProductInterface>(`${this.REST_API_SERVER}/${id}`);
   }
 }
 
